7fa0b6236fc2f7364978614a4db6697a2492dc05
Surmann Roland <surmannroland@gmail.com>
2021-06-23 15:12:37 +0200

    Dto-k egy része megvan

---
+++ backend/UnderSea.Bll/Dtos/UpgradeDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderSea.Bll.Dtos
+{
+    public class UpgradeDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "A fejlesztés nevét kötelező megadni!")]
+        [StringLength(100, ErrorMessage = "A fejlesztés neve maximum 100 karakter hosszú lehet!")]
+        public string Name { get; set; }
+        public bool DoesExist { get; set; }
+        public bool IsUnderConstruction { get; set; }
+        public int EstimatedTime { get; set; }
+        public ICollection<EffectDto> Effects { get; set; }
+    }
+}
