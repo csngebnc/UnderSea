ba51f08127b1c2cf820a938ccb8879132bd9f54f
Csönge Bence <bluebence@gmail.com>
2021-06-23 16:20:15 +0200

    User és Upgrade Controller

--- backend/UnderSea.Api/Controllers/UserController.cs
+++ backend/UnderSea.Api/Controllers/UserController.cs
@@ -4,6 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UnderSea.Bll.Dtos;
+using UnderSea.Bll.Paging;
+using UnderSea.Dal.Data;
 
 namespace UnderSea.Api.Controllers
 {
@@ -11,5 +14,47 @@ namespace UnderSea.Api.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        public UserController()
+        {
+
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(RegisterDto registerDto)
+        {
+            return Ok("success: register");
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(LoginDto registerDto)
+        {
+            return Ok("success: login");
+        }
+
+        [HttpGet("ranklist")]
+        public async Task<ActionResult<PagedResult<UserRankDto>>> GetRanklist(PaginationData pagination) 
+        {
+            return Ok(new PagedResult<UserRankDto>());
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<UserInfoDto>> GetUserInfo()
+        {
+            return Ok(new UserInfoDto { Id = new Guid().ToString("N"), Name = "Teszt User", Placement = 1, Round = 1 });
+        }
+
+        [HttpGet("details")]
+        public async Task<ActionResult<UserDetailsDto>> GetUserDetails()
+        {
+            return Ok(new UserDetailsDto());
+        }
+
+        [HttpPut("country-name")]
+        public async Task<ActionResult<string>> ChangeCountryName([FromBody] string countryName)
+        {
+            return Ok($"success: {countryName}");
+        }
+
+
     }
 }
