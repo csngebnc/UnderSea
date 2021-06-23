c67c1700ca73c28b4bdebe4f2b080bb654e2c52d
NoszDom <noszlopi.dominik@gmail.com>
2021-06-23 13:00:23 +0200

    Infobar kész

--- frontend/angular/under-sea/src/app/components/infobar/infobar.component.ts
+++ frontend/angular/under-sea/src/app/components/infobar/infobar.component.ts
@@ -20,12 +20,28 @@ export class InfobarComponent implements OnInit {
   };
 
   resources: Resources = {
-    units: [],
-    buildings: [],
-    buildingsUnderConstruction: [],
+    units: [
+      { id: 1, name: 'shark', count: 10 },
+      { id: 1, name: 'shark', count: 10 },
+      { id: 1, name: 'shark', count: 10 },
+    ],
+    buildings: [
+      { id: 1, name: 'shark', count: 11 },
+      { id: 2, name: 'shark', count: 12 },
+    ],
+    buildingsUnderConstruction: [
+      { id: 1, name: 'shark', count: 1 },
+      { id: 1, name: 'shark', count: 0 },
+    ],
     shells: 123,
     shellsPerRound: 12,
     corals: 126,
-    coralsPerRound: 32
+    coralsPerRound: 32,
+  };
+
+  countUnderConstrction(id: number): number{
+    let result = this.resources.buildingsUnderConstruction.find(b => b.id === id);
+    if(!result) return 0;
+    else return result.count;
   }
 }
--- frontend/angular/under-sea/src/app/models/resource.model.ts
+++ frontend/angular/under-sea/src/app/models/resource.model.ts
@@ -1,5 +1,5 @@
 export type Resource = {
     id: number;
     name: string;
-    count: string;
+    count: number;
 }
