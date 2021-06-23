0a2abd1badfbce426c763bcdaf1d0f3b164cbc57
Surmann Roland <surmannroland@gmail.com>
2021-06-23 17:04:57 +0200

    Servicek elkészítése implementáció nélkül

---
+++ backend/UnderSea.Bll/Services/BattleService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnderSea.Dal.Data;
+
+namespace UnderSea.Bll.Services
+{
+    public class BattleService
+    {
+        private readonly UnderSeaDbContext _context;
+
+        public BattleService(UnderSeaDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ backend/UnderSea.Bll/Services/UpgradeService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnderSea.Dal.Data;
+
+namespace UnderSea.Bll.Services
+{
+    public class UpgradeService
+    {
+        private readonly UnderSeaDbContext _context;
+        public UpgradeService(UnderSeaDbContext context)
+        {
+            _context = context;
+        }
+     }
+}
