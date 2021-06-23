3c08db7c8d2dcfc74b5b695baafae706baedf612
Surmann Roland <surmannroland@gmail.com>
2021-06-23 08:57:09 +0200

    Konstansok kiszervezve, AttackUnit javítása

--- backend/UnderSea.Model/Models/AttackUnit.cs
+++ backend/UnderSea.Model/Models/AttackUnit.cs
@@ -11,7 +11,7 @@ namespace UnderSea.Model.Models
     {
         public int Id { get; set; }
         [Range(0, int.MaxValue, ErrorMessage = "A támadó egység száma nem lehet negatív szám!")]
-        [Required(ErrorMessage = "A támadó egység számának kötelező megadni!")]
+        [Required(ErrorMessage = "A támadó egység számát kötelező megadni!")]
         public int Count { get; set; }
 
         public int AttackId { get; set; }
