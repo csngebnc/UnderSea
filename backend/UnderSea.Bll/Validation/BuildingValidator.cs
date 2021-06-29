cfab995410326f79716ea68ce1a6000397dbeed2
Surmann Roland <surmannroland@gmail.com>
2021-06-29 13:24:15 +0200

    Első validátor kész

--- backend/UnderSea.Bll/Validation/BuildingValidator.cs
+++ backend/UnderSea.Bll/Validation/BuildingValidator.cs
@@ -1,18 +1,26 @@
 ﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnderSea.Bll.Dtos;
+using UnderSea.Dal.Data;
 
 namespace UnderSea.Bll.Validation
 {
     public class BuildingValidator : AbstractValidator<BuyBuildingDto>
     {
-        public BuildingValidator()
+        private readonly UnderSeaDbContext _context;
+
+        public BuildingValidator(UnderSeaDbContext context)
         {
-            RuleFor(x => x.BuildingId).NotNull();
+            this._context = context;
+            RuleFor(building => building.BuildingId).NotNull().MustAsync(async (buildingId, cancellation) =>await BuildingExist(buildingId));
         }
+
+        private async Task<bool> BuildingExist(int buildingId) 
+            => await _context.Buildings.Where(b => b.Id == buildingId).AnyAsync();
     }
 }
