dcc58b55f8a0b2c0dbbe648a411a3a187b348dc9
Bence Csönge <61698833+csngebnc@users.noreply.github.com>
2021-07-01 08:23:04 +0200

    DeveloperExceptionPage engedélyezése
    Mindenre 500-as hiba jön, ezért be lesz kapcsolva a részletesebb hibaleírás miatt.

--- backend/UnderSea.Api/Startup.cs
+++ backend/UnderSea.Api/Startup.cs
@@ -144,7 +144,8 @@ namespace UnderSea.Api
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseDeveloperExceptionPage();
+                //app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
