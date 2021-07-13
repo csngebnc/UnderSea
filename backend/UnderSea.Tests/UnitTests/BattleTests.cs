91e08ee3b012a4b83dc25effe92ce861e5c7e6c2
Surmann Roland <surmannroland@gmail.com>
2021-07-13 17:12:46 +0200

    Unit tesztek

--- backend/UnderSea.Tests/UnitTests/BattleTests.cs
+++ backend/UnderSea.Tests/UnitTests/BattleTests.cs
@@ -19,6 +19,7 @@ using UnderSea.Bll.Paging;
 using UnderSea.Bll.Services;
 using UnderSea.Bll.Services.Interfaces;
 using UnderSea.Dal.Data;
+using UnderSea.Model.Enums;
 using UnderSea.Model.Models;
 using Xunit;
 
@@ -181,6 +182,122 @@ namespace UnderSea.Tests.UnitTests
 
         }
 
+        [Fact]
+        public async Task GetLoggedAttacksTest()
+        {
+            // Arrange
+            var expectedAttacks = new List<Attack>
+            {
+                new Attack
+                {
+                    AttackerCountryId = 1,
+                    AttackRound = 1,
+                    DefenderCountryId = 4,
+                    WinnerId = null,
+                    AttackUnits = null
+                },
+                new Attack
+                {
+                    AttackerCountryId = 1,
+                    AttackRound = 1,
+                    DefenderCountryId = 5,
+                    WinnerId = LoggedInUserId,
+                    AttackUnits = null
+                },
+                new Attack
+                {
+                    AttackerCountryId = 1,
+                    AttackRound = 1,
+                    DefenderCountryId = 6,
+                    WinnerId = null,
+                    AttackUnits = null
+                }
+            };
+            PaginationData data = new PaginationData();
 
+            _context.Attacks.AddRange(expectedAttacks);
+            await _context.SaveChangesAsync();
+
+            // Act
+            var actualAttacks = await _service.GetLoggedAttacksAsync(data);
+
+            // Assert
+            actualAttacks.AllResultsCount.Should().Be(expectedAttacks.Count);
+            actualAttacks.Results.Any(attack => attack.Outcome == FightOutcome.CurrentUser).Should().BeTrue();
+            actualAttacks.Results.Any(attack => attack.Outcome == FightOutcome.OtherUser).Should().BeFalse();
+            actualAttacks.PageNumber.Should().Be(data.PageNumber);
+            actualAttacks.PageSize.Should().Be(data.PageSize);
+        }
+
+        [Fact]
+        public async Task GetLoggedSpyReportsTest()
+        {
+            // Arrange
+            var expectedReports = new List<SpyReport>
+            {
+                new SpyReport
+                {
+                    SpySenderCountryId = 1,
+                    Round = 1,
+                    SpiedCountryId = 3,
+                    WinnerId = LoggedInUserId,
+                    NumberOfSpies = 2,
+                    DefensePoints = 4567
+                },
+                new SpyReport
+                {
+                    SpySenderCountryId = 1,
+                    Round = 1,
+                    SpiedCountryId = 5,
+                    WinnerId = LoggedInUserId,
+                    NumberOfSpies = 6,
+                    DefensePoints = 1000
+                },
+                new SpyReport
+                {
+                    SpySenderCountryId = 1,
+                    Round = 1,
+                    SpiedCountryId = 6,
+                    WinnerId = null,
+                    NumberOfSpies = 10,
+                    DefensePoints = 0
+                }
+            };
+            PaginationData data = new PaginationData();
+
+            _context.SpyReports.AddRange(expectedReports);
+            await _context.SaveChangesAsync();
+
+            // Act
+            var actualReports = await _service.GetLoggedSpyReportsAsync(data);
+
+            // Assert
+            actualReports.AllResultsCount.Should().Be(expectedReports.Count);
+            actualReports.Results.Any(spy => spy.OutCome == FightOutcome.CurrentUser).Should().BeTrue();
+            actualReports.Results.Any(spy => spy.OutCome == FightOutcome.OtherUser).Should().BeFalse();
+            actualReports.Results.Any(spy => spy.DefensePoints == 4567).Should().BeTrue();
+            actualReports.PageNumber.Should().Be(data.PageNumber);
+            actualReports.PageSize.Should().Be(data.PageSize);
+        }
+
+        [Fact]
+        public async Task GetAllUnitsTest()
+        {
+            // Arrange
+            var expectedUnits = await _context.Units.ToListAsync();
+
+            // Act
+            var actualUnits = await _service.GetAllUnitsAsync();
+
+            // Assert
+            actualUnits.Count().Should().Be(expectedUnits.Count);
+        }
+
+        [Fact]
+        public async Task GetSpiesTest()
+        {
+            // Arrange
+            //var spy = await 
+        }
     }
 }
