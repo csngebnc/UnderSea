a69a2a0f472b3924039663c732dd5d32337fb87e
Csönge Bence <bluebence@gmail.com>
2021-07-15 13:44:07 +0200

    CountryUnit seed

---
+++ backend/UnderSea.Dal/EntityConfigurations/CountryUnitEntityConfiguration.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnderSea.Model.Models;
+
+namespace UnderSea.Dal.EntityConfigurations
+{
+    public class CountryUnitEntityConfiguration : IEntityTypeConfiguration<CountryUnit>
+    {
+        public void Configure(EntityTypeBuilder<CountryUnit> builder)
+        {
+            builder.HasData(
+                new CountryUnit()
+                {
+                    UnitId = 1,
+                    CountryId = 1,
+                    Count = 10,
+                    BattlesPlayed = 0
+                },
+                 new CountryUnit()
+                 {
+                     UnitId = 1,
+                     CountryId = 1,
+                     Count = 6,
+                     BattlesPlayed = 3
+                 },
+                  new CountryUnit()
+                  {
+                      UnitId = 1,
+                      CountryId = 1,
+                      Count = 17,
+                      BattlesPlayed = 5
+                  },
+                   new CountryUnit()
+                   {
+                       UnitId = 2,
+                       CountryId = 1,
+                       Count = 4,
+                       BattlesPlayed = 0
+                   },
+                    new CountryUnit()
+                    {
+                        UnitId = 2,
+                        CountryId = 1,
+                        Count = 10,
+                        BattlesPlayed = 5
+                    },
+                     new CountryUnit()
+                     {
+                         UnitId = 3,
+                         CountryId = 1,
+                         Count = 10,
+                         BattlesPlayed = 4
+                     },
+                     new CountryUnit()
+                     {
+                         UnitId = 4,
+                         CountryId = 1,
+                         Count = 10,
+                         BattlesPlayed = 0
+                     },
+                     new CountryUnit()
+                     {
+                         UnitId = 5,
+                         CountryId = 1,
+                         Count = 10,
+                         BattlesPlayed = 0
+                     }
+                );
+        }
+    }
+}
---
+++ backend/UnderSea.Dal/Migrations/20210715114030_CountryUnitSeed.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UnderSea.Dal.Migrations
+{
+    public partial class CountryUnitSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "CountryMaterials",
+                keyColumns: new[] { "CountryId", "MaterialId" },
+                keyValues: new object[] { 1, 2 },
+                columns: new[] { "Amount", "BaseProduction" },
+                values: new object[] { 1000, 200 });
+
+            migrationBuilder.InsertData(
+                table: "CountryUnits",
+                columns: new[] { "BattlesPlayed", "CountryId", "UnitId", "Count" },
+                values: new object[,]
+                {
+                    { 0, 1, 1, 10 },
+                    { 3, 1, 1, 6 },
+                    { 5, 1, 1, 17 },
+                    { 0, 1, 2, 4 },
+                    { 5, 1, 2, 10 },
+                    { 4, 1, 3, 10 },
+                    { 0, 1, 4, 10 },
+                    { 0, 1, 5, 10 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "CountryUnits",
+                keyColumns: new[] { "BattlesPlayed", "CountryId", "UnitId" },
+                keyValues: new object[] { 0, 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "CountryUnits",
+                keyColumns: new[] { "BattlesPlayed", "CountryId", "UnitId" },
+                keyValues: new object[] { 3, 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "CountryUnits",
+                keyColumns: new[] { "BattlesPlayed", "CountryId", "UnitId" },
+                keyValues: new object[] { 5, 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "CountryUnits",
+                keyColumns: new[] { "BattlesPlayed", "CountryId", "UnitId" },
+                keyValues: new object[] { 0, 1, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "CountryUnits",
+                keyColumns: new[] { "BattlesPlayed", "CountryId", "UnitId" },
+                keyValues: new object[] { 5, 1, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "CountryUnits",
+                keyColumns: new[] { "BattlesPlayed", "CountryId", "UnitId" },
+                keyValues: new object[] { 4, 1, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "CountryUnits",
+                keyColumns: new[] { "BattlesPlayed", "CountryId", "UnitId" },
+                keyValues: new object[] { 0, 1, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "CountryUnits",
+                keyColumns: new[] { "BattlesPlayed", "CountryId", "UnitId" },
+                keyValues: new object[] { 0, 1, 5 });
+
+            migrationBuilder.UpdateData(
+                table: "CountryMaterials",
+                keyColumns: new[] { "CountryId", "MaterialId" },
+                keyValues: new object[] { 1, 2 },
+                columns: new[] { "Amount", "BaseProduction" },
+                values: new object[] { 0, 0 });
+        }
+    }
+}
